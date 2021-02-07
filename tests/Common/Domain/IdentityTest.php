5ed40139cf4f0f704acc29d6458a666a83199786
adam <a.zajac@createit.com>
2021-02-07 19:40:11 +0100

    identity test changes

--- tests/Common/Domain/IdentityTest.php
+++ tests/Common/Domain/IdentityTest.php
@@ -5,14 +5,45 @@ namespace App\Tests\Common\Domain;
 
 
 use App\Common\Domain\Identity;
+use Ramsey\Uuid\Exception\InvalidUuidStringException;
 use Ramsey\Uuid\Uuid;
 
 class IdentityTest extends \PHPUnit\Framework\TestCase
 {
+    public function testIdentityCreation()
+    {
+        // given
+        $stringId = Uuid::uuid4();
+        $stringId = $stringId->toString();
+
+        // when
+        $identity = new Identity($stringId);
+
+        // then
+
+        $this->assertInstanceOf(Identity::class, $identity);
+
+    }
+
+    public function testItFailsIdentityCreation()
+    {
+        $this->expectException(InvalidUuidStringException::class);
+
+        // given
+        $stringId = 'dupa';
+
+        // when
+        $identity = new Identity($stringId);
+
+        // then
+
+    }
+
     public function testIdentityComparision()
     {
         // given
         $uuid = Uuid::uuid4();
+        $uuid = $uuid->toString();
         $firstIdentity = new Identity($uuid);
         $secondIdentity = new Identity($uuid);
 
